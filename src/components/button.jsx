ec075ab4d2a5b0d107d4a36f34e7b4ae39b5ff97
Naman1369 <naman.binu.s73@kalvium.community>
2025-02-22 19:14:36 +0530

    assignment completed

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,13 @@
 // write button card here
+
+import React from "react";
+
+const ViewProductButton = () => {
+  return (
+    <button className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
+      View Product
+    </button>
+  );
+};
+
+export default ViewProductButton;
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,23 @@
 // write product card here
+
+import React from "react";
+import ViewProductButton from "../components/button"; // Corrected path
+;
+
+const Productcard = () => {
+
+  const productImage = "https://www.apple.com/newsroom/images/2024/09/apple-introduces-iphone-16-and-iphone-16-plus/article/Apple-iPhone-16-hero-240909_inline.jpg.large_2x.jpg";
+  const productName = "iphone 16";
+  const productPrice = "$799.99";
+
+  return (
+    <div className="border rounded-lg shadow-md p-4 flex flex-col items-center space-y-3">
+      <img src={productImage} alt={productName} className="w-32 h-32 object-cover" />
+      <h2 className="text-lg font-semibold">{productName}</h2>
+      <p className="text-gray-700 font-medium">{productPrice}</p>
+      <ViewProductButton />
+    </div>
+  );
+};
+
+export default Productcard;
